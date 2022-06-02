9722a496b1606653061cd0bb024ac0ed3f214f29
Juan Vizarro <gvr.stroke@gmail.com>
2022-06-02 13:35:14 -0500

    feat: add tests

---
+++ __test__/buttons/PButton.test.ts
@@ -0,0 +1,25 @@
+import { describe,it, expect } from "vitest"
+import { shallowMount, config } from "@vue/test-utils"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import PButton from "../../src/components/buttons/PButton.vue"
+// const PButton = defineComponent(()=> import("../../components/buttons/PButton.vue"))
+
+const createComponent = (data: any = {}) => shallowMount(PButton, data)
+
+config.global.components = {
+  PButton,
+  Fa: FontAwesomeIcon,
+}
+// config.global.plugins = [ i18n ]
+
+describe("PoliButton",()=>{
+  
+  it("should render primary button",() => {
+    const wrapper = createComponent({
+      props: {
+        type: "primary"
+      }
+    })
+    expect(wrapper.classes()).toContain("btn-primary")
+  })
+})
