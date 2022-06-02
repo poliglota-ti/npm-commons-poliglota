8121392f1325c2f567d73ea1ef87fb9fb7ff95eb
Juan Vizarro <gvr.stroke@gmail.com>
2022-06-02 13:44:45 -0500

    feat: add project interfaces

---
+++ src/interfaces/Nav.ts
@@ -0,0 +1,4 @@
+export interface NavItem {
+  id: number | string
+  text: string
+}
---
+++ src/interfaces/Tooltip.ts
@@ -0,0 +1,6 @@
+export interface TooltipDirectiveBinding {
+  value: string
+  modifiers: {
+    [key: string]: boolean
+  }
+}
