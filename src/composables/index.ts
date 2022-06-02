6a0a08674de885db5ab6333a4f2d950d1fb2d270
Juan Vizarro <gvr.stroke@gmail.com>
2022-06-02 13:37:49 -0500

    feat: init composables

---
+++ src/composables/index.ts
@@ -0,0 +1,5 @@
+import useVModel from "./useVModel"
+
+export {
+  useVModel
+}
---
+++ src/composables/useVModel.ts
@@ -0,0 +1,28 @@
+import { computed, getCurrentInstance } from "vue";
+
+import type { WritableComputedRef } from "vue";
+
+/**
+ * composable que sirve para crear variables que permitan jugar con v-model
+ * @param props variable "props" que resulta de usar defineProps
+ * @param valueName el nombre de la propiedad que se usará par ael v-model, por  defecto es modelValue
+ * @returns {WritableComputedRef<any>}
+ */
+const useVModel = (valueName = "modelValue") => {
+  const currentInstance = getCurrentInstance();
+  const emit = currentInstance?.emit ?? (() => {});
+  const props = currentInstance?.props ?? {}
+
+  const value = computed<any>({
+    get() {
+      return props[valueName]
+    },
+    set(value) {
+      const emitName = `update:${valueName}`
+      emit(emitName, value)
+    },
+  });
+  return value
+};
+
+export default useVModel
