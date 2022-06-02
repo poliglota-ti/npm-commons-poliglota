cdb721616a72d3dc044b4ef233b8908688b880ea
Juan Vizarro <gvr.stroke@gmail.com>
2022-06-02 13:36:10 -0500

    feat: improve vite config

---
+++ vite.config.ts
@@ -0,0 +1,69 @@
+/// <reference types="vitest" />
+
+import { resolve } from "path"
+
+import { defineConfig } from "vite"
+
+import vue from "@vitejs/plugin-vue"
+import typescript2 from "rollup-plugin-typescript2"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    typescript2({
+      check: false,
+      include: ["src/components/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+        exclude: [
+          "vite.config.ts",
+          "src/main.ts",
+          "src/router.ts",
+          "src/views/*.vue",
+          "src/plugins/*.ts",
+          "*.test.ts",
+        ],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(resolve(__dirname), "src"),
+      },
+      {
+        find: "~bootstrap",
+        replacement: "bootstrap",
+      },
+    ]
+  },
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "./src/plugin.ts",
+      formats: ["es", "cjs"],
+      name: "npmCommonsPoliglota",
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  }
+})
