d4ff7540f160bf983f35dd6b4bbd3bddfd35cad1
Juan Vizarro <gvr.stroke@gmail.com>
2022-07-05 11:28:20 -0500

    feat: improve component

--- src/components/cards/PCard.vue
+++ src/components/cards/PCard.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
   interface Props {
     backgroundColor?: string,
+    outlined?: boolean,
   }
   const props = withDefaults(defineProps<Props>(),{
-    backgroundColor: 'bg-white'
+    backgroundColor: 'bg-white',
+    outlined: false,
   })
 </script>
 <template>
   <div
-    class="shadow-sm rounded d-flex gap-3 p-3"
-    :class="[props.backgroundColor]"
+    class="shadow-sm rounded d-flex gap-3 p-3 p-card"
+    :class="[props.backgroundColor, props.outlined?'border':'']"
   >
     <slot></slot>
   </div>
 </template> 
+<style lang="scss" scoped>
+  .p-card {
+    &.border{
+      border: 1px solid var(--bs-gray-100)!important;
+    }
+  }
+</style>
