6fe4a31ad759f569c6b68e3eb8f3fbebd7fa38a5
Juan Vizarro <gvr.stroke@gmail.com>
2022-07-05 11:29:16 -0500

    feat: import new components

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,5 +1,6 @@
 import type { App, DefineComponent } from "vue";
-import { components } from "./components";
+// import { components } from "./components";
+import { PButton, PAlert, PAvatar, PNav, PCard, PStars, PTag } from "./components";
 
 // composables
 import { useVModel } from "./composables";
@@ -15,11 +16,19 @@ export default {
       logoImg: null,
     }
   ) => {
-    Object.keys(components).forEach((key) => {
-      const component = components[key]
-      app.component(key,component)
-    })
-    
+    // Object.keys(components).forEach((key) => {
+    //   const component = components[key]
+    //   app.component(key,component)
+    // })
+    // install components
+    app.component("PButton", PButton);
+    app.component("PAlert", PAlert);
+    app.component("PAvatar", PAvatar);
+    app.component("PNav", PNav);
+    app.component("PCard", PCard);
+    app.component("PStars", PStars);
+    app.component("PTag",PTag);
+
     // options provided by plugin installation
     app.provide("logoImg", options.logoImg)
   },
@@ -29,4 +38,5 @@ export default {
 export { useVModel };
 
 // exportamos componentes uno por uno
-export const { PButton } = components
+export { PButton, PAlert, PAvatar, PNav, PCard, PStars, PTag };
+// export const { PButton } = components
