0f0ffa7cf6bf217ab0cffbc89886089817e4950a
Juan Vizarro <gvr.stroke@gmail.com>
2022-07-24 19:13:52 -0500

    feat: create checkbox component

---
+++ src/components/form/PCheckbox.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { useVModel } from '@/composables'
+
+  const props = defineProps<{
+    type: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link'
+    toast?: boolean
+  }>()
+  const model = useVModel<boolean>()
+</script>
+
+<template>
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      value=""
+      @input="model = !model"
+      :checked="model"
+    >
+    <!-- <label class="form-check-label" for="flexCheckDefault">
+      Default checkbox
+    </label> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
