a5d09a58185347bbdc9425dc9d728e097feb1d63
Juan Vizarro <gvr.stroke@gmail.com>
2022-08-11 11:12:35 -0500

    feat: add props to custom card

--- src/components/cards/PCard.vue
+++ src/components/cards/PCard.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
   interface Props {
-    backgroundColor?: string,
-    outlined?: boolean,
+    backgroundColor?: string
+    outlined?: boolean
+    paddingClass?: string
   }
   const props = withDefaults(defineProps<Props>(),{
     backgroundColor: 'bg-white',
     outlined: false,
+    paddingClass: 'p-3'
   })
 </script>
 <template>
   <div
-    class="shadow-sm rounded d-flex gap-3 p-3 p-card"
-    :class="[props.backgroundColor, props.outlined?'border':'']"
+    class="shadow-sm rounded d-flex gap-3 p-card"
+    :class="[props.backgroundColor, props.paddingClass, props.outlined ? 'border' : '']"
   >
     <slot></slot>
   </div>
