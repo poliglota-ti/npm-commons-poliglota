ea620b9304dcff4285528939bcf5583aeb5e7794
Juan Vizarro <gvr.stroke@gmail.com>
2022-09-05 10:21:55 -0500

    feat(card): add shadow prop

--- src/components/cards/PCard.vue
+++ src/components/cards/PCard.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
+  import { computed } from "vue"
   interface Props {
     backgroundColor?: string
     outlined?: boolean
     paddingClass?: string
+    shadow?: 'none' | 'sm' | 'md' | 'lg'
   }
   const props = withDefaults(defineProps<Props>(),{
     backgroundColor: 'bg-white',
     outlined: false,
-    paddingClass: 'p-3'
+    paddingClass: 'p-3',
+    shadow: 'sm'
   })
+  const shadowComputed = computed(() =>`shadow-${props.shadow ?? 'md'}`)
 </script>
 <template>
   <div
     class="shadow-sm rounded d-flex gap-3 p-card"
-    :class="[props.backgroundColor, props.paddingClass, props.outlined ? 'border' : '']"
+    :class="[props.backgroundColor, props.paddingClass, props.outlined && 'border', shadowComputed]"
   >
     <slot></slot>
   </div>
